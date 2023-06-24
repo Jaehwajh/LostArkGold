2bffa1ab6a37b097f27920a8854518cd8f5330d1
Jaehwajh <jaehwa.jh@gmail.com>
2023-06-24 14:55:20 -0700

    fixing mongodb

--- config/database.js
+++ config/database.js
@@ -1,18 +1,24 @@
 const { MongoClient } = require("mongodb");
 const mongoose = require("mongoose");
 
-const connectDB = async() => {
-    try {
-        const conn = await MongoClient.connect(process.env.DB_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+// const connectDB = async() => {
+//     try {
+//         const conn = await MongoClient.connect(process.env.DB_STRING, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    }catch(err){
-        console.error(err);
-        process.exit(1);
-    }
+//         console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     }catch(err){
+//         console.error(err);
+//         process.exit(1);
+//     }
+// }
+const dbOptions = {
+    useNewUrlParser: true,  
+    useUnifiedTopology: true,
 }
+
+const connectDB = mongoose.connect(process.env.DB_STING, dbOptions)
 
 module.exports = connectDB;
