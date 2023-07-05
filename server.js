9fcc03cfb71b335c473c31d998bc553d117edbfd
Jaehwa <Jaehwa.jh@gmail.com>
2023-07-05 16:36:00 -0700

    fixing view errors

--- server.js
+++ server.js
@@ -6,14 +6,28 @@ const MongoStore = require("connect-mongo");
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
-const connectDB = require("./config/database");
-const dashboardRouter = require("./routes/dashboard");
 const { MongoClient } = require("mongodb");
+const dashboardRouter = require("./routes/dashboard");
 
-require("dotenv").config({ path: "./config/.env"});
+// Database
+const client = new MongoClient(process.env.DB_STRING);
+
+const connectDB = async() => {
+    try{
+        await client.connect();
+        console.log(`MongoDB Connected!`);
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+};
 
 connectDB();
 
+// Dot env config
+require("dotenv").config({ path: "./config/.env"});
+
+//App view 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -34,19 +48,6 @@ app.use(flash());
 app.listen(process.env.PORT, () => {
     console.log("Test build online")
 });
-
-// Database
-const client = new MongoClient(process.env.DB_STRING);
-
-const connectDB = async() => {
-    try{
-        await client.connect();
-        console.log(`MongoDB Connected!`);
-    }catch(err){
-        console.log(err);
-        process.exit(1);
-    }
-};
 
 // route setup
 app.use("/dashboard", dashboardRouter);
