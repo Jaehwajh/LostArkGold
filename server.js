d4951e98a57da6f80f224378d09ba3a64103d6be
Jaehwa <Jaehwa.jh@gmail.com>
2023-07-06 20:04:41 -0700

    change express for view

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ require("dotenv").config({ path: "./config/.env"});
 
 //App view 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + '../public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
