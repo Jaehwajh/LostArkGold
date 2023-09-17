bca657adc1d2e4313a2f52b884fa33d7bc92533e
Jaehwa <Jaehwa.jh@gmail.com>
2023-09-16 20:03:46 -0700

    created roster model and controller

--- controllers/main.js
+++ controllers/main.js
@@ -3,12 +3,14 @@ const roster = require("../model/roster");
 module.exports = {
     getMainpage: (req, res) => {
             res.render("main.ejs")
-        }
-    },
-    createRoster: async(req, res) => {
+        },
+     createRoster: async(req, res) => {
         try{
             await roster.create({
-                
-            })
+                rosterType: req.body.roster
+            });
+        }catch(err){
+            console.log(err);
         }
-    }
+    },
+}
