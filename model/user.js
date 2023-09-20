8f18624f00ed47c7a82518879cb13c26be37642e
Jaehwa <Jaehwa.jh@gmail.com>
2023-09-20 15:19:38 -0700

    added passport and authentication middleware

---
+++ model/user.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        unique: true
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: String
+});
+
+userSchema.pre("save", function save(next) {
+    const user = this;
+    if (!user.isModified("password")) {
+      return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  });
+
+  userSchema.methods.comparePassword = function comparePassword(
+    candidatePassword,
+    cb
+  ) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      cb(err, isMatch);
+    });
+  };
+
+  module.exports = mongoose.model("User", userSchema);
