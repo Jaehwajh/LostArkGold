e552172c58da1c49d7d76339bae75fadf7b295ac
Jaehwa <Jaehwa.jh@gmail.com>
2023-09-26 15:33:53 -0700

    display brel gold and gate

--- config/legion/brelshaza.js
+++ config/legion/brelshaza.js
@@ -1,15 +1,18 @@
 const normalBrelshaza = {
     itemLevel: [1490,1500,1520],
-    gates: [1,2,3,4],
+    gate: [1,2,3,4],
     gold: [2000,2500,1500,2500],
     chest: [400,600,800,1500]
 };
 
 const hardBrelshaza = {
     itemLevel: [1540,1550,1560],
-    gates: [1,2,3,4],
+    gate: [1,2,3,4],
     gold: [2500,3000,2000,3000],
     chest: [700,900,1100,1800]
 };
+
+const nmBrelGold = normalBrelshaza.gold.reduce((a,b) => a + b);
+const hmBrelGold = hardBrelshaza.gold.reduce((a,b) => a + b);
 
 module.exports = {normalBrelshaza, hardBrelshaza};
