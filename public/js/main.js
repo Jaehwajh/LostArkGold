93620c1210a2532328338cbb483bab922c565c50
Jaehwa <Jaehwa.jh@gmail.com>
2023-10-01 16:37:49 -0700

    added toggle to view raid info

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,12 @@
 // Onclick
+
+document.addEventListener('DOMContentLoaded', () => {
+    const raidCards = document.querySelectorAll('.raid-card');
+  
+    raidCards.forEach((raidCard) => {
+      raidCard.addEventListener('click', (event) => {
+        const raidInfo = raidCard.querySelector('.raid-info');
+        raidInfo.classList.toggle('show');
+      });
+    });
+  });
