a416e223b35b9dd42c71ae3320951c422bb8fe06
Jaehwa <Jaehwa.jh@gmail.com>
2023-10-03 16:36:16 -0700

    added small change to view dashboard and functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,25 @@
 // Onclick
 
-document.addEventListener('DOMContentLoaded', () => {
-    const raidCards = document.querySelectorAll('.raid-card');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const raidCards = document.querySelectorAll('.raid-card');
   
-    raidCards.forEach((raidCard) => {
-      raidCard.addEventListener('click', (event) => {
-        const raidInfo = raidCard.querySelector('.raid-info');
+//     raidCards.forEach((raidCard) => {
+//       raidCard.addEventListener('click', (event) => {
+//         const raidInfo = raidCard.querySelector('.raid-info');
+//         raidInfo.classList.toggle('show');
+//       });
+//     });
+//   });
+
+// More Info
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const moreButtons = document.querySelectorAll('.more');
+  
+    moreButtons.forEach((button) => {
+      button.addEventListener('click', (event) => {
+        const raidInfo = event.target.nextElementSibling;
         raidInfo.classList.toggle('show');
       });
     });
-  });
+  });   
