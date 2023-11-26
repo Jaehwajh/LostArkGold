5d7e24e5868fd2145d7fc03d4271a5b4dd9866d3
Jaehwa <Jaehwa.jh@gmail.com>
2023-11-25 23:51:49 -0800

    preadded voldice abyss dungeon

---
+++ config/abyss/voldice.js
@@ -0,0 +1,15 @@
+const normalVoldice = {
+    itemLevel: 1600,
+    gate: [1,2,3],
+    gold: [],
+    chest: [],
+};
+
+const hardVoldice = {
+    itemLevel : 1620,
+    gate: [1,2,3],
+    gold: [],
+    chest: [],
+};
+
+module.exports  = {normalVoldice, hardVoldice};
