72a5561a2a98d1f92c621ce4d25fd72dca06cfca
Jaehwa <Jaehwa.jh@gmail.com>
2023-12-20 14:37:29 -0800

    adding ivory tower raids to roster dashboard

--- config/abyss/ivorytower.js
+++ config/abyss/ivorytower.js
@@ -1,15 +1,15 @@
 const normalVoldis = {
     itemLevel: 1600,
-    gate: [1,2,3],
-    gold: [],
-    chest: [],
+    gate: [1,2,3,4],
+    gold: [1500,1750,2500,3250],
+    chest: [700,800,900,1100],
 };
 
 const hardVoldis = {
-    itemLevel : 1620,
-    gate: [1,2,3],
-    gold: [],
-    chest: [],
+    itemLevel: 1620,
+    gate: [1,2,3,4],
+    gold: [2000,2500,4000,6000],
+    chest: [1000,1000,1500,2000],
 };
 
 module.exports  = {normalVoldis, hardVoldis};
