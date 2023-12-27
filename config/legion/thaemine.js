cf8001c9560c90a5088c67a55e944ba111ba8d54
Jaehwa <Jaehwa.jh@gmail.com>
2023-12-26 22:18:13 -0800

    added thaemine data

---
+++ config/legion/thaemine.js
@@ -0,0 +1,15 @@
+const normalThaemine = {
+    itemLevel: [1610],
+    gate: [1,2,3],
+    gold: [],
+    chest: []
+};
+
+const hardThaemine = {
+    itemLevel: [1630],
+    gate: [1,2,3,4],
+    gold: [],
+    chest: []
+}
+
+module.exports = {normalThaemine, hardThaemine}
